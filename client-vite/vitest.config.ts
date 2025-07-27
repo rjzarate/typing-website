f9dfe77bd5982c4956ea55a3c7cec9fe587de7f0
Froilan Zarate Jr <zarate_rj@yahoo.com>
2025-07-26 18:10:06 -0700

    workflow fix

--- client-vite/vitest.config.ts
+++ client-vite/vitest.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
             include: ["src/**/*.tsx", "src/**/*.ts"],
             reporter: ["text", "json-summary", "json", "html"],
             reportOnFailure: true,
+            reportsDirectory: "./coverage",
         },
     },
 });
