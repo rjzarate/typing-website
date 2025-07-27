f181db5a2e15653137f4461419b743524fef9a71
Froilan Zarate Jr <zarate_rj@yahoo.com>
2025-07-27 13:40:02 -0700

    switch to pytest

--- api-django/apps/postgres/apps.py
+++ api-django/apps/postgres/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PostgresConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'postgres'
+    name = 'apps.postgres'
--- api-django/apps/postgres/tests.py
+++ api-django/apps/postgres/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from django.test import Client
+from apps.postgres.models import Foo
+from django.core.management import call_command
+from django.db import connection
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_model_foo():
+    foo = Foo(name="Test")
+    assert isinstance(foo, Foo)
+
+@pytest.fixture
+def client():
+    return Client()
+
+@pytest.mark.django_db
+def test_model_instance():
+    foo = Foo.objects.create(name="Foo 0")
+    assert Foo.objects.count() == 1
+
+@pytest.fixture
+def create_foos():
+    Foo.objects.create(name="Foo 1")
+    Foo.objects.create(name="Foo 2")
+
+@pytest.mark.django_db
+def test_get_all_foos(client, create_foos):
+    response = client.get("/postgres/test/")
+    assert response.status_code == 200
+
+    data = response.json()
+    assert len(data) == 2
+    assert any(foo["name"] == "Foo 1" for foo in data)
+    assert not any(foo["name"] == "Foo 3" for foo in data)
