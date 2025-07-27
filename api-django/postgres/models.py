82e2088dc35e5905183f25cd83b4cbdf40695cc6
Froilan Zarate Jr <zarate_rj@yahoo.com>
2025-07-26 20:47:36 -0700

    clarification comments

--- api-django/postgres/models.py
+++ api-django/postgres/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Foo(models.Model):
+    # id = models.IntegerField(primary_key=True) do not need to add cause it's already a primary key
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
